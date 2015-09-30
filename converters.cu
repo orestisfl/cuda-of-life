083179db8b210773d3a27fe3ef0e3e5d54ee5dd9
orestisf <orestisf1993@gmail.com>
2015-09-30 16:05:42 +0300

    Fix checks in converters.cu

--- converters.cu
+++ converters.cu
@@ -9,18 +9,18 @@ void convert_from_tiled(int* d_table, const bboard* d_a, const size_t dim, const
     // coordinates in the tiled table.
     const int major_i = blockIdx.y * blockDim.y + threadIdx.y;
     const int major_j = blockIdx.x * blockDim.x + threadIdx.x;
-    if (major_i * WIDTH > dim) return;
-    if (major_j * WIDTH > dim) return;
+    if (major_i * WIDTH >= dim) return;
+    if (major_j * WIDTH >= dim) return;
 
     const bboard* row_a = (bboard*)((char*)d_a + major_j * pitch);
     const bboard value = row_a[major_i];
 
     for (int board_i = 0; board_i < WIDTH; board_i++){
         const int real_i = major_i * WIDTH + board_i;
-        if (real_i > dim) break;
+        if (real_i >= dim) break;
         for (int board_j = 0; board_j < WIDTH; board_j++){
             const int real_j = major_j * WIDTH + board_j;
-            if (real_j > dim) break;
+            if (real_j >= dim) break;
             const int real_idx = real_i * dim + real_j;
             d_table[real_idx] = BOARD_IS_SET(value, board_i, board_j);
         }
@@ -33,18 +33,19 @@ void convert_to_tiled(const int* d_table, bboard* d_a, const size_t dim, const s
 
     const int major_i = blockIdx.y * blockDim.y + threadIdx.y;
     const int major_j = blockIdx.x * blockDim.x + threadIdx.x;
-    if (major_i * WIDTH > dim) return;
-    if (major_j * WIDTH > dim) return;
+    if (major_i * WIDTH >= dim) return;
+    if (major_j * WIDTH >= dim) return;
+
 
     bboard* row_a = (bboard*)((char*)d_a + major_j * pitch);
     bboard value = 0;
 
     for (int board_i = 0; board_i < WIDTH; board_i++){
         const int real_i = major_i * WIDTH + board_i;
-        if (real_i > dim) break;
+        if (real_i >= dim) break;
         for (int board_j = 0; board_j < WIDTH; board_j++){
             const int real_j = major_j * WIDTH + board_j;
-            if (real_j > dim) break;
+            if (real_j >= dim) break;
             const int real_idx = real_i * dim + real_j;
             if (d_table[real_idx]) {
                 SET_BOARD(value, board_i, board_j);
