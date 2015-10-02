bfe22eeda602d07ed1c03f886961e93775f17626
Christos Tsirigotis <tsirif@gmail.com>
2015-10-02 17:40:12 +0300

    Include dynamically allocated shared memory

--- calculator.cu
+++ calculator.cu
@@ -1,6 +1,7 @@
 #include <cuda_runtime.h>
 #include "utils.h"
 
+#define TPOS(X, Y, DIM)((X) * (DIM) + (Y))
 #define T_I 0
 #define C_I 1
 #define B_I 2
@@ -9,114 +10,172 @@
 #define R_J 2
 
 __global__ void calculate_next_generation(const bboard* d_a,
-                                          bboard* d_result,
-                                          const int dim,
-                                          const int dim_board_w,
-                                          const int dim_board_h,
-                                          const size_t pitch,
-                                          const int remaining_cells_w,
-                                          const int remaining_cells_h
-                                         ) {
-    const int major_j = __mul24(blockIdx.y, blockDim.y) + threadIdx.y;  // col
-    const int major_i = __mul24(blockIdx.x, blockDim.x) + threadIdx.x;  // row
+    bboard* d_result,
+    const int dim,
+    const int dim_board_w,
+    const int dim_board_h,
+    const size_t pitch,
+    const int remaining_cells_w,
+    const int remaining_cells_h
+    ) {
+  const int tx = threadIdx.x;
+  const int ty = threadIdx.y;
+  const int major_j = __mul24(blockIdx.y, blockDim.y) + tx;  // col
+  const int major_i = __mul24(blockIdx.x, blockDim.x) + ty;  // row
 
-    if ((__mul24(major_j, WIDTH) >= dim) || (__mul24(major_i, HEIGHT) >= dim)) return;
+  if ((__mul24(major_j, WIDTH) >= dim) || (__mul24(major_i, HEIGHT) >= dim)) return;
 
-    bboard neighbors[3][3];
-    {
-        const int major_l = (major_j - 1 + dim_board_w) % dim_board_w;
-        const int major_r = (major_j + 1) % dim_board_w;
-        const int major_t = (major_i - 1 + dim_board_h) % dim_board_h;
-        const int major_b = (major_i + 1) % dim_board_h;
-        bboard* row_c = (bboard*)((char*)d_a + major_i * pitch);
-        bboard* row_t = (bboard*)((char*)d_a + major_t* pitch);
-        bboard* row_b = (bboard*)((char*)d_a + major_b * pitch);
-        neighbors[C_I][C_J] = row_c[major_j];
-        neighbors[C_I][L_J] = row_c[major_l];
-        neighbors[C_I][R_J] = row_c[major_r];
-        neighbors[T_I][C_J] = row_t[major_j];
-        neighbors[T_I][L_J] = row_t[major_l];
-        neighbors[T_I][R_J] = row_t[major_r];
-        neighbors[B_I][C_J] = row_b[major_j];
-        neighbors[B_I][L_J] = row_b[major_l];
-        neighbors[B_I][R_J] = row_b[major_r];
+  extern __shared__ bboard tiles[];
+
+  int bx, by;
+  if (blockIdx.x == gridDim.x - 1) {
+    bx = dim_board_h - blockIdx.x * blockDim.x + 2;
+  }
+  else {
+    bx = blockDim.x + 2;
+  }
+  if (blockIdx.y == gridDim.y - 1) {
+    by = dim_board_w - blockIdx.y * blockDim.y + 2;
+  }
+  else {
+    by = blockDim.y + 2;
+  }
+
+  const int major_l = (major_j - 1 + dim_board_w) % dim_board_w;
+  const int major_r = (major_j + 1) % dim_board_w;
+  const int major_t = (major_i - 1 + dim_board_h) % dim_board_h;
+  const int major_b = (major_i + 1) % dim_board_h;
+
+  tiles[TPOS(tx + 1, ty + 1, by)] = (d_a + major_i * pitch)[major_j];
+
+  if (ty == 0) {
+    //is in the left edge of the block. keep row
+    tiles[TPOS(tx + 1, 0, by)] = (d_a + major_i * pitch)[major_l];
+
+    if (tx == 0) {
+      //top left corner!
+      tiles[TPOS(0, 0, by)] = (d_a + major_t * pitch)[major_l];
     }
 
-    const bool is_edge_r = (major_j == dim_board_w - 1);
-    const bool is_edge_d = (major_i == dim_board_h - 1);
-    const bool is_edge_u = (major_i == 0);
-    const bool is_edge_l = (major_j == 0);
+    if (tx == bx - 3) {
+      //bottom left corner
+      tiles[TPOS(bx - 1, 0, by)] = (d_a + major_b * pitch)[major_l];
+    }
+  }
 
-    const char limit_i = HEIGHT - __mul24(remaining_cells_h, is_edge_d);
-    const char limit_j = WIDTH - __mul24(remaining_cells_w, is_edge_r);
+  if (ty == by - 3) {
+    //is on the right edge
+    tiles[TPOS(tx + 1, by - 1, by)] = (d_a + major_i * pitch)[major_r];
 
-    bboard value = 0;
-    for (char i = 0; i < limit_i; i++) {
-        char up_i, up_n, down_i, down_n;
-        char first_cells, second_cells;
-        char alive_cells, this_cell;
-
-        if (i == 0) {
-            up_i = HEIGHT - 1 - remaining_cells_h * is_edge_u;
-            up_n = T_I;
-        } else {
-            up_i = i - 1;
-            up_n = C_I;
-        }
-        if (i == limit_i - 1) {
-            down_i = 0;
-            down_n = B_I;
-        } else {
-            down_i = i + 1;
-            down_n = C_I;
-        }
-
-        for (int j = 0; j < limit_j; j++) {
-            this_cell = BOARD_IS_SET(neighbors[C_I][C_J], i, j);
-            char right_j, right_n;
-
-            if (j == 0) {
-                int left_j = WIDTH - 1 - remaining_cells_w * is_edge_l;
-                first_cells = BOARD_IS_SET(neighbors[up_n][L_J], up_i, left_j)
-                              + BOARD_IS_SET(neighbors[C_I][L_J], i, left_j)
-                              + BOARD_IS_SET(neighbors[down_n][L_J], down_i, left_j);
-                second_cells = BOARD_IS_SET(neighbors[up_n][C_J], up_i, j)
-                               + this_cell
-                               + BOARD_IS_SET(neighbors[down_n][C_J], down_i, j);
-            }
-            if (j == limit_j - 1) {
-                right_j = 0;
-                right_n = R_J;
-            } else {
-                right_j = j + 1;
-                right_n = C_J;
-            }
-
-            if (j & 1u) {
-                alive_cells = second_cells;
-                second_cells = BOARD_IS_SET(neighbors[up_n][right_n], up_i, right_j)
-                               + BOARD_IS_SET(neighbors[C_I][right_n], i, right_j)
-                               + BOARD_IS_SET(neighbors[down_n][right_n], down_i, right_j);
-                alive_cells += second_cells;
-                alive_cells += first_cells - this_cell;
-            } else {
-                alive_cells = first_cells;
-                first_cells = BOARD_IS_SET(neighbors[up_n][right_n], up_i, right_j)
-                              + BOARD_IS_SET(neighbors[C_I][right_n], i, right_j)
-                              + BOARD_IS_SET(neighbors[down_n][right_n], down_i, right_j);
-                alive_cells += first_cells;
-                alive_cells += second_cells - this_cell;
-            }
-
-            const bool set = (alive_cells == 3) || (alive_cells == 2 && this_cell);
-
-            if (set) {
-                SET_BOARD(value, i, j);
-            }
-        }
-
+    if (tx == 0) {
+      // top right corner
+      tiles[TPOS(0, by - 1, by)] = (d_a + major_t * pitch)[major_r];
     }
 
-    bboard* row_result = (bboard*)((char*)d_result + major_i * pitch);
-    row_result[major_j] = value;
+    if (tx == bx - 3) {
+      // bottom right corner
+      tiles[TPOS(bx - 1, by - 1, by)] = (d_a + major_b * pitch)[major_r];
+    }
+  }
+
+  if (tx == 0) {
+    //is on the upper edge of the block. keep col
+    tiles[TPOS(0, ty + 1, by)] = (d_a + major_t * pitch)[major_j];
+  }
+
+  if (tx == bx - 3) {
+    //is on the bottom edge
+    tiles[TPOS(bx - 1, ty + 1, by)] = (d_a + major_b * pitch)[major_j];
+  }
+
+  __syncthreads();
+
+  bboard neighbors[3][3];
+  neighbors[C_I][C_J] = tiles[TPOS(tx + 1, ty + 1, by)];
+  neighbors[C_I][L_J] = tiles[TPOS(tx + 1, ty, by)];
+  neighbors[C_I][R_J] = tiles[TPOS(tx + 1, ty + 2, by)];
+  neighbors[T_I][C_J] = tiles[TPOS(tx, ty + 1, by)];
+  neighbors[T_I][L_J] = tiles[TPOS(tx, ty, by)];
+  neighbors[T_I][R_J] = tiles[TPOS(tx, ty + 2, by)];
+  neighbors[B_I][C_J] = tiles[TPOS(tx + 2, ty + 1, by)];
+  neighbors[B_I][L_J] = tiles[TPOS(tx + 2, ty, by)];
+  neighbors[B_I][R_J] = tiles[TPOS(tx + 2, ty + 2, by)];
+
+  const bool is_edge_r = (major_j == dim_board_w - 1);
+  const bool is_edge_d = (major_i == dim_board_h - 1);
+  const bool is_edge_u = (major_i == 0);
+  const bool is_edge_l = (major_j == 0);
+
+  const char limit_i = HEIGHT - __mul24(remaining_cells_h, is_edge_d);
+  const char limit_j = WIDTH - __mul24(remaining_cells_w, is_edge_r);
+
+  bboard value = 0;
+  for (char i = 0; i < limit_i; i++) {
+    char up_i, up_n, down_i, down_n;
+    char first_cells, second_cells;
+    char alive_cells, this_cell;
+
+    if (i == 0) {
+      up_i = HEIGHT - 1 - remaining_cells_h * is_edge_u;
+      up_n = T_I;
+    } else {
+      up_i = i - 1;
+      up_n = C_I;
+    }
+    if (i == limit_i - 1) {
+      down_i = 0;
+      down_n = B_I;
+    } else {
+      down_i = i + 1;
+      down_n = C_I;
+    }
+
+    for (int j = 0; j < limit_j; j++) {
+      this_cell = BOARD_IS_SET(neighbors[C_I][C_J], i, j);
+      char right_j, right_n;
+
+      if (j == 0) {
+        int left_j = WIDTH - 1 - remaining_cells_w * is_edge_l;
+        first_cells = BOARD_IS_SET(neighbors[up_n][L_J], up_i, left_j)
+          + BOARD_IS_SET(neighbors[C_I][L_J], i, left_j)
+          + BOARD_IS_SET(neighbors[down_n][L_J], down_i, left_j);
+        second_cells = BOARD_IS_SET(neighbors[up_n][C_J], up_i, j)
+          + this_cell
+          + BOARD_IS_SET(neighbors[down_n][C_J], down_i, j);
+      }
+      if (j == limit_j - 1) {
+        right_j = 0;
+        right_n = R_J;
+      } else {
+        right_j = j + 1;
+        right_n = C_J;
+      }
+
+      if (j & 1u) {
+        alive_cells = second_cells;
+        second_cells = BOARD_IS_SET(neighbors[up_n][right_n], up_i, right_j)
+          + BOARD_IS_SET(neighbors[C_I][right_n], i, right_j)
+          + BOARD_IS_SET(neighbors[down_n][right_n], down_i, right_j);
+        alive_cells += second_cells;
+        alive_cells += first_cells - this_cell;
+      } else {
+        alive_cells = first_cells;
+        first_cells = BOARD_IS_SET(neighbors[up_n][right_n], up_i, right_j)
+          + BOARD_IS_SET(neighbors[C_I][right_n], i, right_j)
+          + BOARD_IS_SET(neighbors[down_n][right_n], down_i, right_j);
+        alive_cells += first_cells;
+        alive_cells += second_cells - this_cell;
+      }
+
+      const bool set = (alive_cells == 3) || (alive_cells == 2 && this_cell);
+
+      if (set) {
+        SET_BOARD(value, i, j);
+      }
+    }
+
+  }
+
+  bboard* row_result = (bboard*)((char*)d_result + major_i * pitch);
+  row_result[major_j] = value;
 }
--- main.cu
+++ main.cu
@@ -5,155 +5,155 @@
 #include "utils.h"
 
 static void swap_boards(bboard** a, bboard** b) {
-    bboard* t;
-    t = *a;
-    *a = *b;
-    *b = t;
+  bboard* t;
+  t = *a;
+  *a = *b;
+  *b = t;
 }
 
 static inline void cudaCheckErrors(const char msg[], const char file[], int line) {
-    do {
-        cudaError_t __err = cudaGetLastError();
+  do {
+    cudaError_t __err = cudaGetLastError();
 
-        if (__err != cudaSuccess) {
-            fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n",
-                    msg, cudaGetErrorString(__err),
-                    file, line);
-            cudaDeviceReset();
-            exit(1);
-        }
-    } while (0);
+    if (__err != cudaSuccess) {
+      fprintf(stderr, "Fatal error: %s (%s at %s:%d)\n",
+          msg, cudaGetErrorString(__err),
+          file, line);
+      cudaDeviceReset();
+      exit(1);
+    }
+  } while (0);
 }
 
 #define DEFAULT_OPTY 16
 #define DEFAULT_OPTX 16
 void best_block_size(int* optx, int* opty) {
-    #ifdef CUDA_65
-    // The launch configurator returned block size
-    int block_size = 0;
-    // The minimum grid size needed to achieve the maximum occupancy for a full device launch
-    int min_grid_size = 0;
-    cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
-                                       (void*)calculate_next_generation,  0, 0);
+#ifdef CUDA_OCCOPT
+  // The launch configurator returned block size
+  int block_size = 0;
+  // The minimum grid size needed to achieve the maximum occupancy for a full device launch
+  int min_grid_size = 0;
+  cudaOccupancyMaxPotentialBlockSize(&min_grid_size, &block_size,
+      (void*)calculate_next_generation,  0, 0);
 
-    *optx = (int) ceil(sqrt(block_size));
-    *opty = *optx;
-    #else
-    *optx = DEFAULT_OPTX;
-    *opty = DEFAULT_OPTY;
-    #endif
+  *optx = (int) ceil(sqrt(block_size));
+  *opty = *optx;
+#else
+  *optx = DEFAULT_OPTX;
+  *opty = DEFAULT_OPTY;
+#endif
 }
 
 #ifndef TESTING
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        printf("usage: %s fname dim (iter blockx blocky gridx gridy)\n", argv[0]);
-        exit(1);
-    }
-    int n_runs = DFL_RUNS;
-    if (argc >= 4) {
-        n_runs = atoi(argv[3]);
-    }
-    const int dim = atoi(argv[2]);
-    const size_t total_elements = dim * dim;
-    const size_t mem_size = total_elements * sizeof(int);
-    dim3 block;
-    dim3 grid;
-    const int dim_board_w = CEIL_DIV(dim, WIDTH);
-    const int dim_board_h = CEIL_DIV(dim, HEIGHT);
+  if (argc < 3) {
+    printf("usage: %s fname dim (iter blockx blocky gridx gridy)\n", argv[0]);
+    exit(1);
+  }
+  int n_runs = DFL_RUNS;
+  if (argc >= 4) {
+    n_runs = atoi(argv[3]);
+  }
+  const int dim = atoi(argv[2]);
+  const size_t total_elements = dim * dim;
+  const size_t mem_size = total_elements * sizeof(int);
+  dim3 block;
+  dim3 grid;
+  const int dim_board_w = CEIL_DIV(dim, WIDTH);
+  const int dim_board_h = CEIL_DIV(dim, HEIGHT);
 
-    if (argc >= 7) {
-        block.x = atoi(argv[4]);
-        block.y = atoi(argv[5]);
-        grid.x = atoi(argv[6]);
-        grid.y = atoi(argv[7]);
-    } else {
-        int optx, opty;
-        best_block_size(&optx, &opty);
-        fprintf(stderr, "opt=%d %d\n", optx, opty);
+  if (argc >= 7) {
+    block.x = atoi(argv[4]);
+    block.y = atoi(argv[5]);
+    grid.x = atoi(argv[6]);
+    grid.y = atoi(argv[7]);
+  } else {
+    int optx, opty;
+    best_block_size(&optx, &opty);
+    fprintf(stderr, "opt=%d %d\n", optx, opty);
 
-        block.x = (dim_board_h < optx) ? dim_board_h : optx;
-        block.y = (dim_board_w < opty) ? dim_board_w : opty;
-        grid.x = CEIL_DIV(dim_board_h, block.x);
-        grid.y = CEIL_DIV(dim_board_w, block.y);
-    }
-    cudaFree(0); // init device
+    block.x = (dim_board_h < optx) ? dim_board_h : optx;
+    block.y = (dim_board_w < opty) ? dim_board_w : opty;
+    grid.x = CEIL_DIV(dim_board_h, block.x);
+    grid.y = CEIL_DIV(dim_board_w, block.y);
+  }
+  cudaFree(0); // init device
 
-    //    const int remaining_blocks = remaining_dim / WIDTH; // for shared??
-    const int remaining_dim_h = grid.x * block.x * HEIGHT - dim;
-    const int remaining_dim_w = grid.y * block.y * WIDTH - dim;
-    const int remaining_cells_h = remaining_dim_h % HEIGHT;
-    const int remaining_cells_w = remaining_dim_w % WIDTH;
+  //    const int remaining_blocks = remaining_dim / WIDTH; // for shared??
+  const int remaining_dim_h = grid.x * block.x * HEIGHT - dim;
+  const int remaining_dim_w = grid.y * block.y * WIDTH - dim;
+  const int remaining_cells_h = remaining_dim_h % HEIGHT;
+  const int remaining_cells_w = remaining_dim_w % WIDTH;
 
-    char* filename = argv[1];
-    fprintf(stderr,
-            "%s: Reading %dx%d table from file %s\n", argv[0], dim, dim, filename);
-    fprintf(stderr,
-            "%s: Running on a grid(%d, %d) with a block(%d, %d):\nFilename: %s with dim %d for %d iterations\n",
-            argv[0], grid.x, grid.y, block.x, block.y, filename, dim, n_runs);
-    int* table;
-    table = (int*) malloc(mem_size);
-    read_from_file(table, filename, dim);
-    //    print_table(table, dim);
+  char* filename = argv[1];
+  fprintf(stderr,
+      "%s: Reading %dx%d table from file %s\n", argv[0], dim, dim, filename);
+  fprintf(stderr,
+      "%s: Running on a grid(%d, %d) with a block(%d, %d):\nFilename: %s with dim %d for %d iterations\n",
+      argv[0], grid.x, grid.y, block.x, block.y, filename, dim, n_runs);
+  int* table;
+  table = (int*) malloc(mem_size);
+  read_from_file(table, filename, dim);
+  //    print_table(table, dim);
 
-    int* d_table;
-    cudaMalloc((void**) &d_table,  mem_size);
-    cudaCheckErrors("device allocation of GOL matrix failed", __FILE__, __LINE__);
+  int* d_table;
+  cudaMalloc((void**) &d_table,  mem_size);
+  cudaCheckErrors("device allocation of GOL matrix failed", __FILE__, __LINE__);
 
-    bboard* d_board;
-    bboard* d_help;
-    size_t pitch;
-    cudaMallocPitch((void**)&d_board, &pitch, dim_board_w * sizeof(bboard), dim_board_h);
-    cudaCheckErrors("device pitch allocation of GOL matrix failed", __FILE__, __LINE__);
-    cudaMallocPitch((void**)&d_help, &pitch, dim_board_w * sizeof(bboard), dim_board_h);
-    cudaCheckErrors("device pitch allocation of GOL matrix failed", __FILE__, __LINE__);
+  bboard* d_board;
+  bboard* d_help;
+  size_t pitch;
+  cudaMallocPitch((void**)&d_board, &pitch, dim_board_w * sizeof(bboard), dim_board_h);
+  cudaCheckErrors("device pitch allocation of GOL matrix failed", __FILE__, __LINE__);
+  cudaMallocPitch((void**)&d_help, &pitch, dim_board_w * sizeof(bboard), dim_board_h);
+  cudaCheckErrors("device pitch allocation of GOL matrix failed", __FILE__, __LINE__);
 
-    cudaMemcpy(d_table, table, mem_size, cudaMemcpyHostToDevice);
-    cudaCheckErrors("copy from host to device memory failed", __FILE__, __LINE__);
-    free(table);
+  cudaMemcpy(d_table, table, mem_size, cudaMemcpyHostToDevice);
+  cudaCheckErrors("copy from host to device memory failed", __FILE__, __LINE__);
+  free(table);
 
-    convert_to_tiled <<< grid, block >>> (d_table, d_board, dim, pitch);
-    cudaCheckErrors("convert_to_tiled failed", __FILE__, __LINE__);
+  convert_to_tiled <<< grid, block >>> (d_table, d_board, dim, pitch);
+  cudaCheckErrors("convert_to_tiled failed", __FILE__, __LINE__);
 
-    // start timewatch
-    float time;
-    cudaEvent_t start, stop;
-    cudaEventCreate(&start);
-    cudaEventCreate(&stop);
-    cudaEventRecord(start, 0);
+  // start timewatch
+  float time;
+  cudaEvent_t start, stop;
+  cudaEventCreate(&start);
+  cudaEventCreate(&stop);
+  cudaEventRecord(start, 0);
 
-    //    zero_k <<< 1, 1 >>> (d_table, dim);
-    for (int i = 0; i < n_runs; ++i) {
-        calculate_next_generation <<< grid, block>>> (d_board, d_help,
-                                                      dim, dim_board_w, dim_board_h, pitch,
-                                                      remaining_cells_w, remaining_cells_h
-                                                      );
-        cudaCheckErrors("calculating next generation failed", __FILE__, __LINE__);
-        swap_boards(&d_board, &d_help);
-    }
+  //    zero_k <<< 1, 1 >>> (d_table, dim);
+  for (int i = 0; i < n_runs; ++i) {
+    calculate_next_generation <<< grid, block, (block.x + 2) * (block.y + 2) * sizeof(bboard) >>>
+      (d_board, d_help,
+       dim, dim_board_w, dim_board_h, pitch,
+       remaining_cells_w, remaining_cells_h);
+    cudaCheckErrors("calculating next generation failed", __FILE__, __LINE__);
+    swap_boards(&d_board, &d_help);
+  }
 
-    cudaStreamSynchronize(0);
-    // end timewatch
-    cudaEventRecord(stop, 0);
-    cudaEventSynchronize(stop);
-    cudaEventElapsedTime(&time, start, stop);
-    printf(ANSI_COLOR_RED"CUDA-new"ANSI_COLOR_RESET" time to run: "
-           ANSI_COLOR_RED"%f"ANSI_COLOR_RESET" ms\n", time);
+  cudaStreamSynchronize(0);
+  // end timewatch
+  cudaEventRecord(stop, 0);
+  cudaEventSynchronize(stop);
+  cudaEventElapsedTime(&time, start, stop);
+  printf(ANSI_COLOR_RED"CUDA-new"ANSI_COLOR_RESET" time to run: "
+      ANSI_COLOR_RED"%f"ANSI_COLOR_RESET" ms\n", time);
 
-    convert_from_tiled <<< grid, block >>> (d_table, d_board, dim, pitch);
-    cudaCheckErrors("convert_from_tiled failed", __FILE__, __LINE__);
+  convert_from_tiled <<< grid, block >>> (d_table, d_board, dim, pitch);
+  cudaCheckErrors("convert_from_tiled failed", __FILE__, __LINE__);
 
-    table = (int*) malloc(mem_size);
-    cudaMemcpy(table, d_table, mem_size, cudaMemcpyDeviceToHost);
-    cudaCheckErrors("copy from device to host memory failed", __FILE__, __LINE__);
+  table = (int*) malloc(mem_size);
+  cudaMemcpy(table, d_table, mem_size, cudaMemcpyDeviceToHost);
+  cudaCheckErrors("copy from device to host memory failed", __FILE__, __LINE__);
 
-    //    print_table(table, dim);
-    save_table(table, dim, "test_results.bin");
+  //    print_table(table, dim);
+  save_table(table, dim, "test_results.bin");
 
-    // reset gpu
-    cudaDeviceReset();
+  // reset gpu
+  cudaDeviceReset();
 
-    return 0;
+  return 0;
 }
 
 #endif
